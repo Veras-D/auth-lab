47f9c6ecb51759b0fa09f41be26fa2353078b7d0
Veras-D <dveras2310@gmail.com>
2025-05-29 00:45:54 -0300

    fix(database.ts): Removing decrepeted options

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
