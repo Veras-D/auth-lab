dca1bc64b4d83e857a864d767b965d2ff437f359
Veras-D <dveras2310@gmail.com>
2025-05-29 00:47:00 -0300

    feat(validations): Add JOI validations to routes users and auth

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { profile, logout, refreshToken } from '../controllers/auth.controller';
 import { authenticateToken } from '../middlewares/auth.middleware';
+import { validate } from '../middlewares/validate.middleware';
+import { refreshTokenSchema } from '../validations/auth.validation';
 
 const router = Router();
 
 router.get('/profile', authenticateToken, profile);
 router.post('/logout', authenticateToken, logout);
-router.post('/token/refresh', refreshToken);
+router.post('/token/refresh', validate(refreshTokenSchema, 'body'), refreshToken);
 
 export default router;
