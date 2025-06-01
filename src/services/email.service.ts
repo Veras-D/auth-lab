97e9385f9ddbafc6185cde21f70298c4bb1ee8d7
Veras-D <dveras2310@gmail.com>
2025-06-01 10:06:55 -0300

    feat(service): Add email service for future email validatin and forget password routes

---
+++ src/services/email.service.ts
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export const sendEmail = async (to: string, subject: string, html: string) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
+};
