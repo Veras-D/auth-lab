eafaae04cf03d2a6fd76038f9623840aba980318
Veras-D <dveras2310@gmail.com>
2025-06-26 02:02:17 -0300

    feat(sst): Add sst configuration for AWS deploy

---
+++ sst.config.ts
@@ -0,0 +1,26 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default {
+  app: (input) => ({
+    name: "auth-lab",
+    removal: input.stage === "production" ? "retain" : "remove",
+    protect: input.stage === "production",
+    home: "aws",
+  }),
+
+  async run() {
+    const vpc = new sst.aws.Vpc("AuthVpc");
+    const cluster = new sst.aws.Cluster("AuthCluster", { vpc });
+
+    new sst.aws.Service("AuthService", {
+      cluster,
+      image: "Dockerfile",
+      loadBalancer: {
+        ports: [{ listen: "3000/http" }],
+      },
+      dev: {
+        command: "npm run dev",
+      },
+    });
+  }
+};
